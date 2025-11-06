cf768e756a58d388acf0f6f11cab5c8f80206ecd
kvk55247 <kurapothulavamsi@gmail.com>
2025-11-06 14:35:10 +0530

    terraform

---
+++ 20-bastion/outputs.tf
@@ -0,0 +1,4 @@
+/* 
+output "ssm_parameter_path" {
+  value = "/${var.project}/${var.environment}/public_subnet_ids"
+} */
---
+++ 80-eks/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    type = string
+    default = "roboshop"
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
+
+variable "zone_id" {
+    default = "Z062998929MBH76XZ26W5"
+}
+
+variable "zone_name" {
+    default = "daws84s.info"
+  
+}
